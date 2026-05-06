0b9054b93a1881d5006888693c3603c5c04da7e9
Min-Thant794 <minthanthtun29@gmail.com>
2026-05-06 13:30:18 +0800

    reverted

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,9 +6,9 @@ type UserParams = {
 }
 
 const isValidUUID = (value: string): boolean => {
-    const uuidRegex = 
+    const uuidRegwx =
         /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-    return uuidRegex.test(value);
+    return uuidRegwx.test(value);
 }
 
 export const getUsers = async (_req: Request, res: Response) => {
@@ -22,16 +22,17 @@ export const getUsers = async (_req: Request, res: Response) => {
         const result = await pool.query(query);
 
         return res.status(200).json({
-            message: "Users fetched success",
+            message: "Users fetched successfully",
             count: result.rows.length,
             data: result.rows
-        });
+        })
     } catch (error) {
+        console.error("getUsers error: ", error);
         return res.status(500).json({
             message: "Internal server error"
-        })
+        });
     }
-}
+};
 
 export const getUserById = async (req: Request <UserParams>, res: Response) => {
     try {
@@ -54,15 +55,15 @@ export const getUserById = async (req: Request <UserParams>, res: Response) => {
         if (result.rows.length === 0) {
             return res.status(404).json({
                 message: "User not found!"
-            })
-        };
+            });
+        }
 
         return res.status(200).json({
-            message: "User fetched success",
+            message: "User fetched successfully",
             data: result.rows[0]
         })
     } catch (error) {
-        console.error("getUserById() error: ",error);
+        console.error("getUserById() error: ", error);
         return res.status(500).json({
             message: "Internal server error"
         });
@@ -71,59 +72,59 @@ export const getUserById = async (req: Request <UserParams>, res: Response) => {
 
 export const createUser = async (req: Request, res: Response) => {
     try {
-        const { name, email, hashed_password } = req.body;
+        const { name, email, hashed_password} = req.body;
 
         if (!name || !email || !hashed_password) {
             return res.status(400).json({
-                message: "name, email, password is required!"
+                message: "name, email, and password are required"
             });
         }
 
         const trimmedName = String(name).trim();
-        const trimmedEmail = String(email).trim().toLowerCase();
+        const trimmedEmail = String(email).trim();
         const trimmedHashedPassword = String(hashed_password).trim();
 
         if (!trimmedName || !trimmedEmail || !trimmedHashedPassword) {
             return res.status(400).json({
-                message: "name, email, and password cannot be empty!"
+                message: "name, email, and password cannot be empty."
             })
         }
 
-        const VALUES = [trimmedName, trimmedEmail, trimmedHashedPassword];
         const query = `
             INSERT INTO users (name, email, hashed_password)
             VALUES ($1, $2, $3)
             RETURNING id, name, email, hashed_password, created_at, updated_at
-        `;
+        `
+
+        const VALUES = [trimmedName, trimmedEmail, trimmedHashedPassword];
 
         const result = await pool.query(query, VALUES);
 
         return res.status(200).json({
-            message: "User created success!",
-            count: result.rows.length,
+            message: "User created successfully",
             data: result.rows[0]
-        });
+        })
     } catch (error: any) {
-        if (error.code === "23505") {
+        if (error.code === 23505) {
             return res.status(409).json({
-                message: "Email already exists!"
-            });
-        } else if (error.code === "42601") {
-            return res.status(409).json({
-                message: "SQL syntax error"
+                message: "Email already exist"
             })
+        } else if (error.code === 42601) {
+            return res.status(501).json({
+                message: "SQL syntax error"
+            });
         }
 
         return res.status(500).json({
-            message: "Internal server error"
+            message: "Internal Server Error"
         })
     }
 }
 
-export const updateUser = async (req: Request<UserParams>, res: Response) => {
+export const updateUser = async (req: Request <UserParams>, res: Response) => {
     try {
         const { id } = req.params;
-
+        
         if (!isValidUUID(id)) {
             return res.status(400).json({
                 message: "Invalid user id",
@@ -134,23 +135,25 @@ export const updateUser = async (req: Request<UserParams>, res: Response) => {
         const updates: Record<string, any> = {};
 
         for (const field of allowedFields) {
-            updates[field] = req.body[field]
+            if (req.body[field] !== undefined) {
+                updates[field] = req.body[field];
+            }
         }
 
         const keys = Object.keys(updates);
 
         if (keys.length === 0) {
             return res.status(400).json({
-                message: "No valid fields are provided for update"
+                message: "No valid fields provided for update",
             });
         }
 
         if (updates.name !== undefined) {
-            updates.name = String(updates.name).trim();
+            updates.name = String(updates.name).trim()
             if (!updates.name) {
                 return res.status(400).json({
                     message: "name cannot be empty",
-                })
+                });
             }
         }
 
@@ -158,19 +161,20 @@ export const updateUser = async (req: Request<UserParams>, res: Response) => {
             updates.email = String(updates.email).trim().toLocaleLowerCase();
             if (!updates.email) {
                 return res.status(400).json({
-                    message: "email cannot be empty.",
-                })
+                    message: "email cannot be empty",
+                });
             }
         }
 
         const setClause = keys.map((key, index) => `${key} = $${index + 1}`).join(", ");
 
         const values = keys.map((key) => updates[key]);
+
         values.push(id);
 
         const query = `
             UPDATE users
-            SELECT ${setClause}
+            SET ${setClause}
             WHERE id = $${values.length}
             RETURNING id, name, email, created_at, updated_at
         `;
@@ -180,23 +184,25 @@ export const updateUser = async (req: Request<UserParams>, res: Response) => {
         if (result.rows.length === 0) {
             return res.status(404).json({
                 message: "User not found",
-            })
-        }
-
-        return res.status(200).json({
-            message: "user updated success!",
-            data: result.rows[0]
-        })
-    } catch (error: any) {
-        if (error.code === "23505") {
-            return res.status(409).json({
-                message: "email already exists."
             });
         }
 
-        return res.status(500).json({
-            message: "Internal server error!"
+        return res.status(200).json({
+            message: "User updated successfully",
+            data: result.rows[0],
         })
+
+    } catch (error: any) {
+        if (error.code === "23505") {
+            return res.status(409).json({
+                message: "Email already exists"
+            });
+        }
+
+        console.error("updateUser() error: ", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
     }
 }
 
@@ -204,26 +210,33 @@ export const deleteUser = async (req: Request <UserParams>, res: Response) => {
     try {
         const { id } = req.params;
 
-        if (!isValidUUID(id)) {
+        if (!isValidUUID) {
             return res.status(400).json({
-                message: "Invalid user id"
+                message: "Invalid user id",
             });
         }
 
         const query = `
             DELETE FROM users
             WHERE id = $1
+            RETURNING id, name, email
         `;
 
         const result = await pool.query(query, [id]);
 
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+
         return res.status(200).json({
-            message: "user deleted successfully",
+            message: "User deleted successfully",
             data: result.rows[0]
         });
     } catch (error) {
         console.error("deleteUser() error: ", error);
-        res.status(500).json({
+        return res.status(500).json({
             message: "Internal server error"
         })
     }
